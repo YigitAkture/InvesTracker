9dd6a84c54a2522266d6ddfe9a1fa9ed266caaef
Yigit Akture <220706301@st.maltepe.edu.tr>
2025-11-12 13:55:55 +0300

    Add new app colors

--- inves_tracker/lib/core/constants/app_colors.dart
+++ inves_tracker/lib/core/constants/app_colors.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+
+  // --- Static Colors ---
+  static const Color danger = Color(0xFFDC0E0E);
+  static const Color danger2 = Color(0xFFCB0404);
+  static const Color danger3 = Color(0xFFC34141);
+
+  static const Color success = Color(0xFF78C841);
+  static const Color success2 = Color(0xFF5CB338);
+
+  static const Color warning = Color(0xFFFCCD2A);
+  static const Color warning2 = Color(0xFFFFC145);
+
+  static const Color teal = Color(0xFF00F5FF);
+  static const Color orange = Color(0xFFFF7F3F);
+  static const Color melon = Color(0xFFEDF492);
+  static const Color pink = Color(0xFFE83C91);
+  static const Color purple = Color(0xFF9C50CC);
+
   // --- Light Colors ---
-  static final Color primaryLight = Colors.blue[300]!;
-  static final Color secondaryLight = Colors.orange[600]!;
-  static final Color backgroundLight = Colors.grey[200]!;
+  static const Color primaryLight = Color(0xFF04C5E7); // TODO: CHANGE THIS
+  static const Color secondaryLight = Color(0xFFFA9A00); // TODO: CHANGE THIS
+  static final Color backgroundLight = Color(0xFFEBEBEB); // TODO: CHANGE THIS
+  static const Color backgroundLight2 = Color(0xFF2D2D47); // TODO: CHANGE THIS
+  static const Color foregroundLight = Color(0xFF41415D); // TODO: CHANGE THIS
 
   // --- Dark Colors ---
-  static const Color primaryDark = Colors.blueAccent;
-  static const Color secondaryDark = Colors.orange;
-  static const Color backgroundDark = Color(0xFF121212);
+  static const Color primaryDark = Colors.blueAccent; // TODO: CHANGE THIS
+  static const Color secondaryDark = Colors.orange; // TODO: CHANGE THIS
+  static const Color backgroundDark = Color(0xFF27283D);
+  static const Color backgroundDark2 = Color(0xFF2D2D47);
+  static const Color foregroundDark = Color(0xFF41415D);
 
   // --- Dynamic Getters ---
   static Color primary(BuildContext context) {
@@ -28,5 +50,17 @@ class AppColors {
     return Theme.of(context).brightness == Brightness.dark
         ? backgroundDark
         : backgroundLight;
+  }
+
+  static Color background2(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark
+        ? backgroundDark2
+        : backgroundLight2;
+  }
+
+  static Color foreground(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark
+        ? foregroundDark
+        : foregroundLight;
   }
 }
